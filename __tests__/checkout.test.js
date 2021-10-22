95a10526401203336d2c4e278c07a74c5ca9c572
Jenny Ardrey <jenny@space48.com>
2021-10-22 08:08:09 +0100

    solution using a class

--- __tests__/checkout.test.js
+++ __tests__/checkout.test.js
@@ -1,42 +1,44 @@
-/* 1- As a user, I would like to be able to scan an item using a barcode so its details can be found from products list (test data).
-2 - As a user, I would like to be able to add an item to my basket.
-3 - As a user, I would like to be able to see the total price of all items in my basket.
-4 - As a user, I would like to be able to remove an item from my basket.
- */
-const { orange, kiwi, pineapple, apple, banana, catalogue} = require('../data/data');
-const { scan, basket, addItem, totalPrice, removeItem } = require('../checkout');
-const { expect, it } = require('@jest/globals');
+const {
+  orange,
+  kiwi,
+  pineapple,
+  apple,
+  banana,
+  catalogue,
+} = require("../data/data");
 
-
+//import class instead of functions or object
+const { SelfCheckout } = require("../checkout");
+const { expect, it } = require("@jest/globals");
 
 describe("selfCheckout", () => {
-    it('returns the details of an item from the barcode', () => {
-        expect(scan(orange.barcode)).toBe(orange);
-    });
+  // create new instance of our class to test on
+  const myCheckout = new SelfCheckout();
 
-    it('adds an item to the basket', () => {
-        expect(basket.length).toBe(0);
+  it("returns the details of an item from the barcode", () => {
+    //change all references to methods to reference out new instance via dot notation
+    expect(myCheckout.scan(orange.barcode)).toBe(orange);
+  });
 
-        addItem(kiwi, basket)
+  it("adds an item to the basket", () => {
+    expect(myCheckout.basket.length).toBe(0);
 
-        expect(basket.length).toBe(1)
-    })
+    //remove 'basket' as argument because we no longer need to pass it in
+    myCheckout.addItem(kiwi);
 
-    it('shows the total price', () => {
-        addItem(pineapple, basket)
-        expect(totalPrice(basket)).toBe(105)    
-    })
+    expect(myCheckout.basket.length).toBe(1);
+  });
 
-    it('removes an item from the basket', () => {
-        expect(basket.length).toBe(2);
+  it("shows the total price", () => {
+    myCheckout.addItem(pineapple);
+    expect(myCheckout.totalPrice()).toBe(105);
+  });
 
-        removeItem(kiwi, basket)
-        
-        expect(basket.length).toBe(1)
-    })
+  it("removes an item from the basket", () => {
+    expect(myCheckout.basket.length).toBe(2);
 
+    myCheckout.removeItem(kiwi);
 
-
-
+    expect(myCheckout.basket.length).toBe(1);
+  });
 });
-
--- checkout.js
+++ checkout.js
@@ -1,28 +1,40 @@
-/* 1- As a user, I would like to be able to scan an item using a barcode so its details can be found from products list (test data).
-2 - As a user, I would like to be able to add an item to my basket.
-3 - As a user, I would like to be able to see the total price of all items in my basket.
-4 - As a user, I would like to be able to remove an item from my basket.
+const { catalogue } = require("./data/data");
+
+/*class basic structure to work from
+
+class SelfCheckout {
+
+    constructor() {
+        properties go here
+    }
+
+    methods go here
+}
  */
+class SelfCheckout {
+  //we dont need to pass any parameters into the constructor because every instance of SelfCheckout will start with the same basket value - an empty array.
+  //If we wanted to give the instances any individual properties, like a name or a store in which it is located, we would have that as a parameter on the constructor and set it to a property. Then it would be passed in when we create the instance (see line 16 of tests)
+  constructor() {
+    this.basket = [];
+  }
 
-const { catalogue } = require('./data/data')
+  scan(customerBarcode) {
+    return catalogue.find((item) => item.barcode === customerBarcode);
+  }
 
-let basket = [];
-const scan = function (customerBarcode) {
-    return catalogue.find(item => item.barcode === customerBarcode)
-}
-const addItem = function (item, trolley) {
-    trolley.push(item)
-}
-const totalPrice = function (trolley) {
-    return trolley.reduce((acc, item) => {
-        return acc + item.price
-    }, 0)
-}
-const removeItem = function (product, trolley) {
-     const index = trolley.findIndex(item => item === product);
-    trolley = trolley.splice(index, 1) 
-    // return trolley = trolley.filter(item => item !== product)
+  // we no longer need to pass 'basket' or 'trolley' into our code because the basket is part of the class and can be accessed using 'this'
+  addItem(item) {
+    this.basket.push(item);
+  }
+  totalPrice = function () {
+    return this.basket.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+  };
+  removeItem = function (product) {
+    const index = this.basket.findIndex((item) => item === product);
+    this.basket = this.basket.splice(index, 1);
+  };
 }
 
-
-module.exports = { scan, basket, addItem, totalPrice, removeItem }
+module.exports = { SelfCheckout };
