1f0f6def26db7746144e0754799b83bc666079d7
Jenny Ardrey <jenny@space48.com>
2021-10-22 07:49:36 +0100

    refactored the functions into a plain object

--- __tests__/checkout.test.js
+++ __tests__/checkout.test.js
@@ -3,40 +3,42 @@
 3 - As a user, I would like to be able to see the total price of all items in my basket.
 4 - As a user, I would like to be able to remove an item from my basket.
  */
-const { orange, kiwi, pineapple, apple, banana, catalogue} = require('../data/data');
-const { scan, basket, addItem, totalPrice, removeItem } = require('../checkout');
-const { expect, it } = require('@jest/globals');
-
-
+const {
+  orange,
+  kiwi,
+  pineapple,
+  apple,
+  banana,
+  catalogue,
+} = require("../data/data");
+const { selfCheckout } = require("../checkout");
+const { expect, it } = require("@jest/globals");
 
 describe("selfCheckout", () => {
-    it('returns the details of an item from the barcode', () => {
-        expect(scan(orange.barcode)).toBe(orange);
-    });
+  it("returns the details of an item from the barcode", () => {
+    expect(selfCheckout.scan(orange.barcode)).toBe(orange);
+  });
+  ``;
 
-    it('adds an item to the basket', () => {
-        expect(basket.length).toBe(0);
+  it("adds an item to the basket", () => {
+    expect(selfCheckout.basket.length).toBe(0);
 
-        addItem(kiwi, basket)
+    selfCheckout.addItem(kiwi);
 
-        expect(basket.length).toBe(1)
-    })
+    expect(selfCheckout.basket.length).toBe(1);
+  });
 
-    it('shows the total price', () => {
-        addItem(pineapple, basket)
-        expect(totalPrice(basket)).toBe(105)    
-    })
+  it("shows the total price", () => {
+    selfCheckout.addItem(pineapple);
+    console.log(selfCheckout.basket);
+    expect(selfCheckout.totalPrice()).toBe(105);
+  });
 
-    it('removes an item from the basket', () => {
-        expect(basket.length).toBe(2);
+  it("removes an item from the basket", () => {
+    expect(selfCheckout.basket.length).toBe(2);
 
-        removeItem(kiwi, basket)
-        
-        expect(basket.length).toBe(1)
-    })
+    selfCheckout.removeItem(kiwi);
 
-
-
-
+    expect(selfCheckout.basket.length).toBe(1);
+  });
 });
-
