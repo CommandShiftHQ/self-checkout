d1c8b3df242aa18b876fd7a53ef41f27b016ed5a
Jenny Ardrey <jenny@space48.com>
2021-10-22 08:09:50 +0100

    emptied files as starting point except for data and imports

--- checkout.js
+++ checkout.js
@@ -4,25 +4,6 @@
 4 - As a user, I would like to be able to remove an item from my basket.
  */
 
-const { catalogue } = require('./data/data')
+const { catalogue } = require("./data/data");
 
-let basket = [];
-const scan = function (customerBarcode) {
-    return catalogue.find(item => item.barcode === customerBarcode)
-}
-const addItem = function (item, trolley) {
-    trolley.push(item)
-}
-const totalPrice = function (trolley) {
-    return trolley.reduce((acc, item) => {
-        return acc + item.price
-    }, 0)
-}
-const removeItem = function (product, trolley) {
-     const index = trolley.findIndex(item => item === product);
-    trolley = trolley.splice(index, 1) 
-    // return trolley = trolley.filter(item => item !== product)
-}
-
-
-module.exports = { scan, basket, addItem, totalPrice, removeItem }
+module.exports = {};
